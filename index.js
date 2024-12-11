5d02dc6c877a12e74102b83834476d73e1fc3b85
Mus1c-James <ditmer2@gmail.com>
2024-12-11 03:04:24 +0200

    ДЗ 5. ДЗ 4.1. Робота з prompt

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
 "use strict";
-const userNameInput = prompt('Your Name');
-const userAgeInput = prompt('Enter your Age');
-const userAnimals = confirm('Do you have animals');
 
+let userNameInput = prompt("Your Name:");
 
-if (userAgeInput === null || userAgeInput.trim() === "") {
-  alert('Error: Age is required');
-} else if (isNaN(userAgeInput) || !Number.isInteger(+userAgeInput)) {
-  alert('Error: Invalid Age');
-} else {
-  const age = Math.abs(+userAgeInput); 
-  if (age < 16 || age > 110) {
-    alert('Error: Age must be between 16 and 110');
-  } 
-}
-
-if (userAnimals === true) {
-   userAnimalInput = prompt('What is the name of your animal?');
-}else{
-   userAnimalInput = 'No pet';
-}
- 
-console.log(`Name: ${userNameInput}, Age: ${userAgeInput},Confirm Pet? ${userAnimals}, Your animal: ${userAnimalInput}`);
-
-
-document.getElementById("content").innerHTML =`
-<p>Your Name: ${userNameInput}</p>
-<p>Your Age: ${userAgeInput}</p>
-<p>Your pet: ${userAnimalInput}</p>`;
-   
+if (userNameInput === null || !userNameInput.trim()) {
+  userNameInput = '[noname]';
+} 
+  
+alert(`Hello, ${userNameInput}! How are you?`); 
